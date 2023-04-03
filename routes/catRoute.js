f167f416558cc2ec977bc6930ce07fc8f6e557a6
RiQhy <riku.nokelainen@metropolia.fi>
2023-04-03 09:13:05 +0300

    minor changes

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -5,36 +5,13 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 const catController = require('../controllers/catController');
-const {body} = require('express-validator');
 
-const fileFilter = (req, file, cb) => {
-    const allowedTypes = ['image/jpeg', 'image/png'];
-    if(allowedTypes.includes(file.mimetype)){
-        cb (null, true);
-    } else {
-        cb(null, false);
-    }
-};
-
-const upload = multer({dest: 'uploads/', fileFilter});
+const upload = multer({next: '../uploads'});
 
 router.route('/')
     .get(catController.getCatList)
-    .post(upload.single('cat') , 
-        body('name').isAlphanumeric().isLength({min: 1, max: 30}).escape().trim(),  
-        body('birthdate').isDate(),
-        body('weight').isFloat({min:0.1, max:50}),
-        body('owner').isInt({min:1}),
-        catController.postCat
-    )
-    .put(
-        body('name').isAlphanumeric().isLength({min: 1, max: 30}),  
-        body('birthdate').isDate(),
-        body('weight').isFloat({min:0.1, max:50}),
-        body('owner').isInt({min:1}),
-        catController.putCat
-    );
-
+    .post(upload.single('cat') ,catController.postCat)
+    .put(catController.putCat);
 router.route('/:id')
     .get(catController.getCat)
     .delete(catController.deleteCat);
