30ef433f6d237d61da16382d2f401ec35ed27cad
RiQhy <riku.nokelainen@metropolia.fi>
2023-04-05 09:04:17 +0300

    passport base

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const { Session } = require('inspector');
+const passport = require("./utils/pass");
 const app = express();
 const port = 3000;
 
+const loggedIn = (req,res,next) => {
+  if(req.user){
+    next();
+  } else {
+    res.redirect("/form");
+  }
+}
 const user = {name: 'foo', password: 'bar'};
 let pageViews = 0;
 
@@ -15,6 +23,8 @@ app.set('view engine', 'pug');
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(session({secret: 'afshjgfgdgf899ew', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   //global values are shared between all clients/sessions
@@ -29,27 +39,21 @@ app.get('/', (req, res) => {
 
   res.render('home');
 });
+
 app.get('/form', (req, res) => {
   res.render('form');
 });
-app.get('/secret', (req, res) => {
-  if(req.session.loggedIn ){
-    res.render('secret');
-  } else {
-    res.status(403).send('you must login to see this!');
-  }
- 
+
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
 });
-app.post('/login', (req,res) => {
-  console.log('trying to log in', req.body);
-  if(req.body.username === user.name && 
-    req.body.password === user.password){
-    req.session.loggedIn = true;
-    res.redirect('/secret');
-  } else {
-    res.status(401).send('login failed');
-  }
+
+app.post('/login', passport.authenticate("local", {failureRedirect: "/form"}), 
+(req,res) => {
+  console.log("success");
+  res.redirect("/secret");
 });
+
 app.get('/logout', (req,res) => {
   req.session.loggedIn = false;
   res.redirect('/form');
