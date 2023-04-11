a0cd61036cc6a2ff8fb4c8fe1bcdc71a366f683d
RiQhy <riku.nokelainen@metropolia.fi>
2023-04-11 17:54:57 +0300

    passport assignment

--- utils/passport.js
+++ utils/passport.js
@@ -41,26 +41,49 @@ const getUserLogin = (email) => {
 
 // serialize: store user id in session
 passport.serializeUser((id, done) => {
+  process.nextTick(() => {
+    return done(null, {
+      id: users.user_id,
+      username: users.name,
+      email: users.email
+    }, 
+    {message: 'User serialized'});
+  });
   console.log("serialize", id);
   // serialize user id by adding it to 'done()' callback
+  
 });
 
 // deserialize: get user id from session and get all user data
 passport.deserializeUser(async (id, done) => {
+  id = getUser(id);
+  process.nextTick(() => {
+    return done(null,id,{message: 'User deserialized'});
+  });
   // get user data by id from getUser
   console.log("deserialize", user);
   // deserialize user by adding it to 'done()' callback
+  
 });
 
 passport.use(
   new Strategy((username, password, done) => {
     // get user by username from getUserLogin
+    try {
+      const [user] = await getUserLogin(email);
+    if(user === undefined){
     // if user is undefined
-    // return done(null, false);
+      return done(null, false);
+    }
+    if(user.password !== password){
     // if passwords dont match
-    // return done(null, false);
+    return done(null, false);
+    }
     // if all is ok
-    // return done(null, user.user_id);
+    return done(null, user.user_id);
+    } catch(err){
+      return done(err);
+    }
   })
 );
 
